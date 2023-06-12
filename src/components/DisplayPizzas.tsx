992ab6e355ddf3272a35ad69d6d10dca3d5d7c7d
Mike <spormuv@gmail.com>
2023-06-12 18:16:23 +0300

    fix: display pagination buttons

--- src/components/DisplayPizzas.tsx
+++ src/components/DisplayPizzas.tsx
@@ -40,28 +40,30 @@ const DisplayPizzas: FC<DisplayPizzasProps> = ({
       ))}
 
       {/* Pagination button block */}
-      <div className="pagination">
-        <p className="text">
-          {page}/{totalPages}
-        </p>
-        <button onClick={prevPage} className="page">
-          &larr;
-        </button>
-
-        {[...Array(totalPages).keys()].map(el => (
-          <button
-            onClick={() => setPage(el + 1)}
-            key={el}
-            className={`page ${page === el + 1 ? 'active' : ''}`}
-          >
-            {el + 1}
+      {!!pizzasList.length && (
+        <div className="pagination">
+          <p className="text">
+            {page}/{totalPages}
+          </p>
+          <button onClick={prevPage} className="page">
+            &larr;
           </button>
-        ))}
 
-        <button onClick={nextPage} className="page">
-          &rarr;
-        </button>
-      </div>
+          {[...Array(totalPages).keys()].map(el => (
+            <button
+              onClick={() => setPage(el + 1)}
+              key={el}
+              className={`page ${page === el + 1 ? 'active' : ''}`}
+            >
+              {el + 1}
+            </button>
+          ))}
+
+          <button onClick={nextPage} className="page">
+            &rarr;
+          </button>
+        </div>
+      )}
     </div>
   );
 };
